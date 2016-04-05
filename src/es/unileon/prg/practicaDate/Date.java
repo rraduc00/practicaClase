0e2180a351bdea3ca138508d1d55b2ae24c9f835
Razvan Raducu <rraduc00@estudiantes.unileon.es>
2016-04-05 14:31:03 +0200

    Primeros pasos de la practica. Hecho el build para el ant y las primeras lineas de los archivos .java

---
+++ src/es/unileon/prg/practicaDate/Date.java
@@ -0,0 +1,11 @@
+package es.unileon.prg.practicaDate;
+/**
+ * Clase date.
+ * 
+ * @author Razvan Raducu X5526828C
+ * @version 1.0
+ */
+
+public class Date{
+
+}
---
+++ src/es/unileon/prg/practicaDate/DateErrorException.java
@@ -0,0 +1,7 @@
+package es.unileon.prg.practicaDate;
+
+public class DateErrorException extends Exception {
+	public DateErrorException(String msg){
+		super(msg);
+	}
+}
