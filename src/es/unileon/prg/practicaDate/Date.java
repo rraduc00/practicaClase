d67184e7e266b82e32f824d6730a3980c81ec529
Razvan Raducu <rraduc00@estudiantes.unileon.es>
2016-04-05 21:30:32 +0200

    Hecho algunos apatados del tema for. Date class exercises.

--- src/es/unileon/prg/practicaDate/Date.java
+++ src/es/unileon/prg/practicaDate/Date.java
@@ -88,4 +88,126 @@ public class Date{
 		return(this.day == anotherDate.getDay() && this.month == anotherDate.getMonth() 
 			&& this.year == anotherDate.getYear());
 	}
+
+	public String getMonthName(){
+		String monthName = "";
+		switch(this.month){
+			case 1:
+				month = "January";
+			break;
+			case 2:
+				month = "February";
+			break;
+			case 3:
+				month = "March";
+			break;
+			case 4:
+				month = "April";
+			break;
+			case 5:
+				month = "May";
+			break;
+			case 6:
+				month = "June";
+			break;
+			case 7:
+				month = "July";
+			break;
+			case 8:
+				month = "August";
+			break;
+			case 9:
+				month = "September";
+			break;
+			case 10:
+				month = "October";
+			break;
+			case 11:
+				month = "November";
+			break;
+			case 12:
+				month = "December";
+			break;
+		return month;
+		}
+	}
+
+	//////////// A method that checks if the day of the month is right  ??????
+
+	public String getSpecificMonthName(int month){
+		String monthName = "";
+		switch(month){
+			case 1:
+				month = "January";
+			break;
+			case 2:
+				month = "February";
+			break;
+			case 3:
+				month = "March";
+			break;
+			case 4:
+				month = "April";
+			break;
+			case 5:
+				month = "May";
+			break;
+			case 6:
+				month = "June";
+			break;
+			case 7:
+				month = "July";
+			break;
+			case 8:
+				month = "August";
+			break;
+			case 9:
+				month = "September";
+			break;
+			case 10:
+				month = "October";
+			break;
+			case 11:
+				month = "November";
+			break;
+			case 12:
+				month = "December";
+			break;
+		return month;
+		}
+	}
+
+	public String getMonthSeason(){
+		String monthSeason ="";
+		switch(this.month){
+			case 3: case 4: case 5:
+				monthSeason = "Spring";
+			break;
+			case 6: case 7: case 8:
+				monthSeason = "Summer";
+			break;
+			case 9: case 10: case 11:
+				monthSeason = "Autumn";
+			break;
+			case 12: case 1: case 2:
+				monthSeason = "Winter";
+			break;
+		}
+	}
+
+	public String getMonthsLeft(){
+		Stringbuffer monthsLeft = new StringBuffer();
+		for(int i = month; i < 13; i++ ){
+			monthsLeft.append(getSpecificMonthName(i));
+		}
+		return monthsLeft.toString();
+	}
+
+
+	/////// For a date, print all dates until the end of the month.  ??????????
+	
+
+	public String toString(){
+		return "The date is: " + day + " of " + month + " of year " + year;
+	}
 }
